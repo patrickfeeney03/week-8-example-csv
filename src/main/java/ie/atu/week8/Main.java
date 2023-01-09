632219b836074870362320dcef7a0b4de5e045e4
Patrick <g00409592@atu.ie>
2023-01-09 17:53:56 +0000

    Added code to main to print text from a .csv file. The elements need to be separated by a comma.

--- src/main/java/ie/atu/week8/Main.java
+++ src/main/java/ie/atu/week8/Main.java
@@ -6,6 +6,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception
     {
-
+        Scanner sc = new Scanner(new File("names.csv"));
+        sc.useDelimiter(",");
+        while (sc.hasNext())
+        {
+            System.out.println(sc.next());
+        }
+        sc.close();
     }
 }
